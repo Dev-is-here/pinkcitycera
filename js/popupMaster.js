e5e11bf0f9ff28982841e8664a6ef85f9d4c80a5
KunalVerma <shreyash4330s1@gmail.com>
2021-08-07 15:53:29 +0530

    initial commit Sat, Aug  7, 2021  3:53:28 PM

---
+++ js/popupMaster.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+			    //return item.el.attr('title') + '<small>by DentCare</small>';
+			    return '<small>by DentCare</small>';
+			}
+		}
+	});
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps, .popup-PDF').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
